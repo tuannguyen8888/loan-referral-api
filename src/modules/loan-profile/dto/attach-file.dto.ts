bc63e9f3ca4b833cbc68fd4af3ceaf308f4c05e3
Võ Tuấn Nguyên <tuannguyen8888@gmail.com>
2021-03-04 15:49:53 +0700

    update-attach-files

--- src/modules/loan-profile/dto/attach-file.dto.ts
+++ src/modules/loan-profile/dto/attach-file.dto.ts
@@ -1,20 +1,21 @@
 import {IsString, IsNotEmpty, IsNumber, IsOptional, IsDateString, IsDefined} from "class-validator";
 
 export class AttachFileDto {
-  id?: number;
-  @IsDefined()
-  loan_profile_id: number;
-  doc_code: string;
-  @IsOptional()
-  @IsDateString()
-  created_at: string;
-  created_by: number;
-  @IsOptional()
-  @IsDateString()
-  updated_at?: string;
-  updated_by?: number;
-  @IsOptional()
-  @IsDateString()
-  deleted_at?: string;
-  deleted_by?: number;
+    id?: number;
+    @IsDefined()
+    loan_profile_id: number;
+    doc_code: string;
+    url: string;
+    @IsOptional()
+    @IsDateString()
+    created_at: string;
+    created_by: number;
+    @IsOptional()
+    @IsDateString()
+    updated_at?: string;
+    updated_by?: number;
+    @IsOptional()
+    @IsDateString()
+    deleted_at?: string;
+    deleted_by?: number;
 }
