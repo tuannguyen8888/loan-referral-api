d278d0c25a3ba3c6064a50898324a9c39b605dd8
nguyen.vo <nguyen.vo@finviet.com.vn>
2021-04-16 08:28:54 +0000

    Merge branch 'dev' into 'test'
    Dev > test
    
    See merge request finance-portal/loan-referral-api!103
    
    (cherry picked from commit 996c64ce74bd7e5c70e71cad221cafe91f77291e)
    
    7fe1b57a [LR] change master data sec user
    386914c7 [LR] fixed master data

--- src/entities/mafc-masterdata/masterdata-secuser.ts
+++ src/entities/mafc-masterdata/masterdata-secuser.ts
@@ -2,7 +2,10 @@ import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("mafc_masterdata_secuser")
 export class SecUserMasterData {
-  @PrimaryColumn("varchar", { name: "lsu_USER_ID_C", length: 255 })
+  @PrimaryColumn("varchar", { name: "id", length: 255 })
+  id: string;
+
+  @Column("varchar", { name: "lsu_USER_ID_C", length: 255 })
   lsu_USER_ID_C: string;
 
   @Column("varchar", { name: "lsu_USER_NAME_C", length: 255 })
