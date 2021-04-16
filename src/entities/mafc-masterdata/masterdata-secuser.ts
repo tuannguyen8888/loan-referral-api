386914c7ca2faf689031591296b43a05d634cbc7
nhvuong1995 <hoangvuong.nguyen>
2021-04-16 15:27:38 +0700

    [LR] fixed master data

--- src/entities/mafc-masterdata/masterdata-secuser.ts
+++ src/entities/mafc-masterdata/masterdata-secuser.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("mafc_masterdata_secuser")
 export class SecUserMasterData {
-  @PrimaryColumn("binary", { name: "id", length: 255 })
+  @PrimaryColumn("varchar", { name: "id", length: 255 })
   id: string;
 
   @Column("varchar", { name: "lsu_USER_ID_C", length: 255 })
