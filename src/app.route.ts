99e438b6818be0fd10957dc267d5be038a9a47fa
Võ Tuấn Nguyên <tuannguyen8888@gmail.com>
2021-04-27 08:54:52 +0700

    add route

--- src/app.route.ts
+++ src/app.route.ts
@@ -3,6 +3,8 @@ import { LoanProfileModule, SaleGroupModule } from "./modules";
 import { MasterDataModule } from "./modules/master-data/master-data.module";
 import { ReceiveResultModule } from "./modules/receive-result/receive-result.module";
 import {PtfLoanProfileModule} from "./modules/ptf/ptf-loan-profile/ptf-loan-profile.module";
+import {PtfMasterDataModule} from "./modules/ptf/ptf-master-data/ptf-master-data.module";
+import {PtfReceiveResultModule} from "./modules/ptf/ptf-receive-result/ptf-receive-result.module";
 
 export const ROUTES: Routes = [
     {
@@ -17,7 +19,9 @@ export const ROUTES: Routes = [
     {
         path: "/api/ptf",
         children: [
-            PtfLoanProfileModule
+            PtfMasterDataModule,
+            PtfLoanProfileModule,
+            PtfReceiveResultModule,
         ]
     },
 ];
