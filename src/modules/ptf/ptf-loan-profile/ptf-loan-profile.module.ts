2b68d167bf8422a65e187156373dd8afc778c96d
Võ Tuấn Nguyên <tuannguyen8888@gmail.com>
2021-04-27 09:51:10 +0700

    fix bug module

--- src/modules/ptf/ptf-loan-profile/ptf-loan-profile.module.ts
+++ src/modules/ptf/ptf-loan-profile/ptf-loan-profile.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {HttpModule, Module} from '@nestjs/common';
 import { PtfLoanProfileController } from './ptf-loan-profile.controller';
 import { PtfLoanProfileService } from './ptf-loan-profile.service';
+import {Logger} from "../../../common/loggers";
+import {RedisClient} from "../../../common/shared";
+import {BaseService} from "../../../common/services";
+import {LoanProfileService} from "../../loan-profile/loan-profile.service";
+import {RequestUtil} from "../../../common/utils";
 
 @Module({
+  imports: [HttpModule],
   controllers: [PtfLoanProfileController],
-  providers: [PtfLoanProfileService]
+  providers: [Logger, RedisClient, BaseService, PtfLoanProfileService, RequestUtil]
 })
 export class PtfLoanProfileModule {}
--- src/modules/ptf/ptf-receive-result/ptf-receive-result.module.ts
+++ src/modules/ptf/ptf-receive-result/ptf-receive-result.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {HttpModule, Module} from '@nestjs/common';
 import { PtfReceiveResultController } from './ptf-receive-result.controller';
 import { PtfReceiveResultService } from './ptf-receive-result.service';
+import {Logger} from "../../../common/loggers";
+import {RedisClient} from "../../../common/shared";
+import {BaseService} from "../../../common/services";
+import {LoanProfileService} from "../../loan-profile/loan-profile.service";
+import {RequestUtil} from "../../../common/utils";
 
 @Module({
+  imports: [HttpModule],
   controllers: [PtfReceiveResultController],
-  providers: [PtfReceiveResultService]
+  providers: [Logger, RedisClient, BaseService, PtfReceiveResultService, RequestUtil]
 })
 export class PtfReceiveResultModule {}
