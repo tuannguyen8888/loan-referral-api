6735f286eae5a2175556eeaec9ccc53b62ea6a29
ho.lu <luthietho@gmail.com>
2021-10-14 17:02:31 +0700

    Update MC

---
+++ src/modules/mc/mc-case-note/mc-case-note.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { McCaseNoteService } from './mc-case-note.service';
+
+describe('McCaseNoteService', () => {
+  let service: McCaseNoteService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [McCaseNoteService],
+    }).compile();
+
+    service = module.get<McCaseNoteService>(McCaseNoteService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
