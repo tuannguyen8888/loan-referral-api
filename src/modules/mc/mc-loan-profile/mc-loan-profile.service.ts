ed15a6e48d28b15d07973f243a8dc698a20a53f7
ho.lu <luthietho@gmail.com>
2021-10-28 15:48:29 +0700

    Update MC

--- src/modules/mc/mc-loan-profile/mc-loan-profile.service.ts
+++ src/modules/mc/mc-loan-profile/mc-loan-profile.service.ts
@@ -21,36 +21,21 @@ import {Logger} from "../../../common/loggers";
 import {RedisClient} from "../../../common/shared";
 import {RequestUtil} from "../../../common/utils";
 import {
-    PtfAddressRepository,
-    PtfLoanProfileRepository,
-    SaleGroupRepository,
-    PtfEmploymentInformationRepository,
-    PtfRelatedPersonRepository,
-    PtfAttachFileRepository,
+
     SendDataLogRepository,
-    ProcessRepository,
-    PtfLoanProfileDeferRepository
+
 } from "../../../repositories";
 import {In, IsNull, Like} from "typeorm";
 import * as moment from "moment";
 import {
-    Address,
-    LoanProfile,
     McLoanProfile,
-    Process,
-    PtfAddress,
-    PtfAttachFile,
-    PtfEmploymentInformation,
-    PtfLoanProfile,
-    PtfLoanProfileDefer,
-    PtfRelatedPerson,
     SendDataLog
 } from "../../../entities";
 import * as FormData from "form-data";
 import * as fs from "fs";
 import * as config from "config";
 import {ProcessDto} from "../../mafc/loan-profile/dto";
-import {GetPtfLoanProfilesRequestDto} from "../../ptf/ptf-loan-profile/dto";
+
 import {McLoanProfileRepository} from "../../../repositories/mc/mc-loan-profile.repository";
 import {CheckInitContractRequestDto} from "./dto/check-init-contract.request.dto";
 import {McCheckListrequestDto} from "./dto/mc-check-listrequest.dto";
@@ -95,7 +80,7 @@ export class McLoanProfileService extends BaseService {
             result.count = count;
             result.rows = this.convertEntities2Dtos(
                 data,
-                PtfLoanProfile,
+                McLoanProfile,
                 LoanProfileResponseDto
             );
             return result;
