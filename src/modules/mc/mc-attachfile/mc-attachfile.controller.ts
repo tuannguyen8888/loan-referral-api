738927bc8f62e42ef9385a6b8633c0f3075daa7a
ho.lu <luthietho@gmail.com>
2021-11-19 21:33:54 +0700

    Update MC

--- src/modules/mc/mc-attachfile/mc-attachfile.controller.ts
+++ src/modules/mc/mc-attachfile/mc-attachfile.controller.ts
@@ -1,6 +1,6 @@
 import {
   Body,
-  Controller,
+  Controller, Delete,
   Get,
   Headers,
   HttpCode,
@@ -21,6 +21,7 @@ import { McAttachfilesResponseDto } from "./dto/mc-attachfiles.response.dto";
 import { McAttachfileResponseDto } from "./dto/mc-attachfile.response.dto";
 import { McAttachfileDto } from "./dto/mc-attachfile.dto";
 import { McAttachfileUpdateDto } from "./dto/mc-attachfile.update.dto";
+import {McAttachfileDeleteDto} from "./dto/mc-attachfile.delete.dto";
 
 @Controller("mc-attachfile")
 export class McAttachfileController {
@@ -61,5 +62,13 @@ export class McAttachfileController {
     @Body() dto: McAttachfileUpdateDto
   ): Promise<McAttachfileUpdateDto> {
     return this.service.updateAttachfile(dto);
+  }
+  @Delete("/")
+  @ApiOperation({ summary: "Xóa Attachfile" })
+  deleteAttachfile(
+      @Headers() headers,
+      @Body() dto: McAttachfileDeleteDto
+  ): Promise<any> {
+    return this.service.deleteAttachfile(dto);
   }
 }
